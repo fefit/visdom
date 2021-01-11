3a2f398f63c23e87069a3181106a035b519f8029
ganmin <ganmin@360.cn>
2021-01-11 18:46:08 +0800

    add implement "is"

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,32 @@
 
 use std::cell::RefCell;
 use std::rc::Rc;
-use rphtml::parser::{Attr, AttrData, Doc, Node, NodeType};
+use rphtml::parser::{Attr, AttrData, CodePosAt, Doc, Node, NodeType};
+use std::result::Result as StdResult;
+use std::error::Error;
 use ntree::selector::interface::{ INodeType, INodeTrait, IAttrValue, BoxDynNode, MaybeResult, Result, NodeList };
-pub struct Visdom{
+use ntree;
+/// type implement INodeTrait with Node
+struct Dom{
   node: Rc<RefCell<Node>>
 }
 
-impl INodeTrait for Visdom {
+fn to_static_str(orig: String)->&'static str{
+  Box::leak(orig.into_boxed_str())
+}
+
+impl INodeTrait for Dom {
   /// impl `cloned`
   fn cloned(&self) -> Box<dyn INodeTrait> {
-    Box::new(Visdom{
+    Box::new(Dom{
       node: self.node.clone()
     })
   }
   /// impl `tag_name`
   fn tag_name(&self) -> &str {
-    if let Some(meta) = self.node.borrow().meta{
-      return meta.borrow().get_name(false).as_str();
+    if let Some(meta) = &self.node.borrow().meta{
+      let name = meta.borrow().get_name(false);
+      return to_static_str(name);
     }
     panic!("Wrong `tag_name`")
   }
@@ -34,9 +43,9 @@ impl INodeTrait for Visdom {
   }
   /// impl `parent`
   fn parent<'b>(&self) -> MaybeResult<'b> {
-    if let Some(parent) = self.node.borrow().parent{
+    if let Some(parent) = &self.node.borrow().parent{
       if let Some(node) = parent.upgrade(){
-        let cur = Visdom{
+        let cur = Dom{
           node 
         };
         return Ok(Some(Box::new(cur)));
@@ -48,11 +57,11 @@ impl INodeTrait for Visdom {
   }
   /// impl `children`
   fn children<'b>(&self) -> Result<'b> {
-    if let Some(childs) = self.node.borrow().childs{
+    if let Some(childs) = &self.node.borrow().childs{
       let mut result = NodeList::with_capacity(childs.len());
       for cur in childs{
         if cur.borrow().node_type == NodeType::Tag{
-          result.get_mut_ref().push(Box::new(Visdom{
+          result.get_mut_ref().push(Box::new(Dom{
             node: cur.clone()
           }));
         }
@@ -63,13 +72,13 @@ impl INodeTrait for Visdom {
   }
   /// impl `get_attribute`
   fn get_attribute(&self, name: &str) -> Option<IAttrValue> {
-     if let Some(meta) = self.node.borrow().meta{
+     if let Some(meta) = &self.node.borrow().meta{
        for attr in &meta.borrow().attrs{
         if let Some(key) = &attr.key{
           if key.content == name{
             if let Some(value) = &attr.value{
               let attr_value = value.content.clone();
-              return Some(IAttrValue::Value(attr_value, attr.quote.clone()));
+              return Some(IAttrValue::Value(attr_value, attr.quote));
             } else {
               return Some(IAttrValue::True);
             }
@@ -83,10 +92,11 @@ impl INodeTrait for Visdom {
   fn set_attribute(&mut self, name: &str, value: Option<&str>) {
     let mut need_quote = false;
     let mut quote:char = '"';
-    if let Some(meta) = self.node.borrow().meta{     
+    let pos = CodePosAt::default();
+    if let Some(meta) = &self.node.borrow().meta{     
       let value = value.map(|v|{
         let mut find_quote: bool = false;
-        let mut result = String::with_capacity(v.len());
+        let mut content = String::with_capacity(v.len());
         // loop the chars
         for ch in v.chars(){
           if !need_quote{
@@ -96,7 +106,7 @@ impl INodeTrait for Visdom {
             if find_quote{
               if quote == ch{
                 // find more quotes
-                result.push('\\');
+                content.push('\\');
               }
             }else{
               // if first is double quote, change the variable `quote` to single quote 
@@ -106,21 +116,79 @@ impl INodeTrait for Visdom {
               }
             }
           }
-          result.push(ch);
+          content.push(ch);
+        }
+        AttrData{
+          content,
+          begin_at: pos,
+          end_at: pos
         }
       });
+      // first, check if the attribute has exist.
       for attr in &mut meta.borrow_mut().attrs{
         if let Some(key) = &attr.key{
           if key.content == name{
-            
+            // find the attribute
+            attr.value = value;
             return;
           }
         }
       }
+      // new attribute, add it to queue.
+      let quote = if value.is_some(){
+        Some(quote)
+      } else {
+        None
+      };
+      meta.borrow_mut().attrs.push(Attr{
+        key: Some(AttrData{
+          content: name.into(),
+          begin_at: pos,
+          end_at: pos
+        }),
+        value,
+        quote,
+        need_quote
+      });
+      return;
     }
     panic!("Can't apply 'set_attribute' to node");
   }
+  /// impl `uuid`
+  fn uuid(&self) -> Option<&str> {
+    if let Some(uuid) = &self.node.borrow().uuid{
+      return Some(to_static_str(uuid.clone()));
+    }
+    None
+  }
 }
-impl Visdom{
 
+impl From<Rc<RefCell<Node>>> for Dom{
+  fn from(node: Rc<RefCell<Node>>) -> Self {
+    Dom{
+      node: Rc::clone(&node)
+    }
+  }
+}
+
+pub struct Vis{
+  pub doc: Doc
+}
+
+impl Vis{
+  pub fn init(){
+    ntree::init();
+  }
+  pub fn load(html: &str)-> StdResult<Vis, Box<dyn Error>>{
+    let doc = Doc::parse(html, Default::default())?;
+    Ok(Vis{
+      doc
+    })
+  }
+  // 
+  pub fn find(&self, query: &str) -> Result{
+    let root: Dom = Rc::clone(&self.doc.root).into();
+    let root_nodes = NodeList::with_nodes(vec![Box::new(root)]);
+    root_nodes.find(query)
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use std::error::Error;
 
-fn main() {
-    println!("Hello, world!");
+use visdom::Vis;
+fn main()->Result<(), Box<dyn Error>> {
+    Vis::init();
+    let html = r##"
+        <div id="hello" class="hello">world</div>
+        <div><span class="hello other"></span></div>
+    "##;
+    let root = Vis::load(html)?;
+    let dom_hello = root.find("[class^='hello']");
+    println!("结果：{:?}", dom_hello?.count());
+    Ok(())
 }
